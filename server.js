e729f52ac3e0d705c542ce58cd86fdfccf0503e6
soora-txt <carolsalkmim@gmail.com>
2026-04-03 11:42:15 -0300

    feat: adiciona busca por ID (GET) e cadastro de incicdentes (POST)

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get("/", (req, res) => {
             <p> Endpoint que leva aos incidentes cadastrados: /incidentes </p>
         </body>
 
-        `);
+        `); 
 });
 
 
@@ -45,6 +45,39 @@ app.get("/incidentes", async (req,res) => {
 
 });
 
+
+//Rota Específica
+
+app.get("/incidentes/:id", async (req,res) => {
+
+    const { id } = req.params
+
+    const db = await criarBanco ()
+
+    const incidentesEspecifico = await db.all(`SELECT * FROM incidentes WHERE id = ?`, [id])
+    // ? é un espaço reservado que será preenchido pelo valor da variável [id]
+    // ? SQL Injection é usado para segurança
+
+    res.json(incidentesEspecifico)
+} )
+
+
+//Rota POST - Novos Registro /Endpoints
+
+app.post("/incidentes", async (req,res) => {
+
+    const {tipo_problema, localizacao, descricao, prioridade, nome_solicitante, data_registro, hora_registro} = req.body
+
+    const db = await criarBanco()
+
+    await db.run(`INSERT INTO incidentes(tipo_problema, localizacao, descricao, prioridade, nome_solicitante, data_registro, hora_registro) VALUES (?, ?, ?, ?, ?, ?, ?)`, [tipo_problema, localizacao, descricao, prioridade, nome_solicitante, data_registro, hora_registro])
+
+    //Envia uma resposta de confirmação para o cliente que fez a requisição
+    res.send(`Incidente novo registrado ${tipo_problema} registado na data ${data_registro} por ${nome_solicitante} `)
+
+})
+
+   
 
 
 
